a05d14a399eb2e347f5b0e803bf6ac6a9efd6da1
DJ Versoza <djversoza@gmail.com>
2017-08-19 19:08:52 -0700

    adding app

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,14 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/spotographer'
-  }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    }
+  }  
 
 };
